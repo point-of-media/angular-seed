97960ac03b96db2f4d7802088c59325067a78045
Frank <frank.boeddeker@gmx.net>
2016-03-07 13:25:28 +0100

    add exampleComponent module to app module

--- app/app.ts
+++ app/app.ts
@@ -2,6 +2,8 @@ import * as angular from 'angular';
 
 import exampleComponent from './scripts/components/example/example-component';
 
-angular.module('app', ['exampleComponent']);
+angular.module('app', [
+    exampleComponent
+]);
 
 angular.bootstrap(document.documentElement, ['app']);
